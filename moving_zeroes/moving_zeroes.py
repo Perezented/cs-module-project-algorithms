1ea43f42b3e2d2f3299e11c37f8520619c519a05
Michael Perez <mperez6473@gmail.com>
2020-07-22 15:24:45 -0500

    Working on moving zeros. Have something set up that moves quite a bit of them but not all the tests pass.

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,9 +2,16 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
-
+    print(arr)
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            print('here is one')
+            arr.append(arr.pop(i))
+    return arr
     pass
 
 
