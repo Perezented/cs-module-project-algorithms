e57385539b69efc3b7b15c09dd5e28acc39d95b9
Michael Perez <mperez6473@gmail.com>
2020-07-23 14:26:47 -0500

    Followed lecture for eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,28 +4,27 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n, filler=None):
+def eating_cookies(n, cache=None):
     # Your code here
-    print(n)
-    print(filler)
-    if filler is None:
-        filler = []
-        returnMe = 0
-    if n <= 1:
+    if cache is None:
+        cache = {}
+    if n < 0:
+        return 0
+    if n == 0:
         return 1
-    if n == 2:
-        return 2
+    # check the cache to see if it holds the answer this branch is looking for
+    elif n in cache:
+        return cache[n]
+    # if n == 2:
+    #     return 2
     else:
-        for num in range(n+1):
-            filler.append(num)
-        for x in filler:
-            for y in filler:
-                if x == n:
-                    returnMe += 1
-                if x + y == n:
-                    returnMe += 1
-    print(returnMe + 1)
-    return returnMe + 1
+        cache[n] = eating_cookies(
+            n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+        return cache[n]
+
+    # use a data structure to 'cache or save' answers taht some other branch has already done
+    # save the n value as a key and the associated answer as its value in a dict
+    # we will need to pass the dictionary to each call
 
 
 if __name__ == "__main__":
@@ -33,4 +32,4 @@ if __name__ == "__main__":
     num_cookies = 5
 
     print(
-        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+        f"There are {eating_cookies(num_cookies), {}} ways for Cookie Monster to each {num_cookies} cookies")
